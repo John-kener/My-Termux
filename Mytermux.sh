99e687c47ccb6270f15ce5c5faea468b95ded01e
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-14 21:22:29 +0530

    Create Mytermux.sh

---
+++ Mytermux.sh
@@ -0,0 +1,111 @@
+echo
+echo
+echo -e "\e[1;33m INSTALLING REQUIREMENTS "
+echo
+echo
+pkg install figlet
+pkg install lolcat
+figlet "over"|lolcat
+sleep 2
+clear    
+
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'                                                      lb='\e[1;36m'
+gr='\e[1;30m'
+wh='\e[1;37m'
+
+
+echo -e "\e[1;35m ________________________________________________________________________"
+echo -e $b"
+>=>       >=> >=>      >=>
+>> >=>   >>=>  >=>    >=>
+>=> >=> > >=>   >=> >=>
+>=>  >=>  >=>     >=>
+>=>   >>  >=>     >=>
+>=>       >=>     >=>
+>=>       >=>     >=>
+"
+echo -e $b"
+######## ######## ########  ##     ## ##     ## ##     ##
+   ##    ##       ##     ## ###   ### ##     ##  ##   ##             ##    ##       ##     ## #### #### ##     ##   ## ##
+   ##    ######   ########  ## ### ## ##     ##    ###
+   ##    ##       ##   ##   ##     ## ##     ##   ## ##
+   ##    ##       ##    ##  ##     ## ##     ##  ##   ##
+   ##    ######## ##     ## ##     ##  #######  ##     ##
+   "
+echo -e "              Created By JOHN KENER "
+echo -e "              @ CYBER WARRIORS "
+echo -e "\e[1;35m ________________________________________________________________________"
+echo -e "\e[1;35m________________________________________________________________________"                                                                                                                          
+echo
+echo -e "\e[1;33m                 [1]  THEME එකක් දාගන්න   ";                                                                                                                                                             
+echo -e "\e[1;33m                 [2]  ASCII ARTS FOR YOUR BANNER  ";
+echo -e "\e[1;33m                 [3]  ඔයාගෙම බැනර් එකක්  හදා ගන්න ";                                                                                                                                                             
+echo -e "\e[1;33m                 [4]  EVIL -EYE බැනර් එක දාගන්න  ";
+echo -e "\e[1;33m                 [5]  ABOUT US  ";
+echo -e "\e[1;33m                 [6]  EXIT             ";  
+echo
+
+echo -e "\e[1;33m NOTE : අනිත් ඒවා කරන්න කලින් ,Theme එකක් අනිවා දාගෙන ඉන්න......!!! "
+echo
+read -p' OPTION එකක් තෝරන්න :- ' userinput1;
+if [ "${userinput1:-}" = "1" ]
+then
+  cd && cd My-Termux && cd Thm && bash install.sh
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+      cd && cd My-Termux && cd ban && bash Asciiarts.sh
+fi
+if [ "${userinput1:-}" = "3" ]
+then
+      cd && cd My-Termux && cd ban && bash mybanner.sh
+fi
+if [ "${userinput1:-}" = "4" ]
+then
+    cd && rm -rf Evil-Eye-Banner && git clone https://github.com/John-kener/Evil-Eye-Banner && cd Evil-Eye-Banner && chmod +x * && bash requirement.sh && bash Evil-banner.sh
+fi
+if [ "${userinput1:-}" = "5" ]
+then 
+echo -e """\e[1;32m 
+
+             +-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+             |C|y|b|e|r| |W|a|r|r|i|o|r|s|
+             +-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+_______________________________________________________
+|                                                     |
+|                                                     |
+| [--] CREATED BY: JOHN KENER(OWNER OF CYBER WARRIORS)|
+|
+|                                                     |
+|
+| [--] Don't copyright this tool ..                   |
+|
+|                                                     |
+|
+:::::::::::::::::::::JOIN WITH US ::::::::::::::::::::|
+|                                                     |
+| [--] Telegram : https://t.me/By_sstp                |
+|                                                     |
+|                                                     |
+| [--] Whatsapp group 2 :                             |
+|  https://chat.whatsapp.com/DWMOhdZv78RHfYpmVfjBuS   |
+|                                                     |
+|                                                     |
+| [--] Youtube channel:                               |
+|http://m.youtube.com/channel/UCOC4YlK-7mb5jIbCRcuijvQ|
+|                                                     |
+|_____________________________________________________|
+
+"""
+
+fi
+if [ "${userinput1:-}" = "6" ]
+then
+figlet THANK YOU  ...! |lolcat
+    cd
+fi
+
