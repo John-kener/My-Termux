fe485bc580fb599bb8a480292232140d81681126
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-15 14:13:02 +0530

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,14 @@
+figlet wait
+pkg update && pkg upgrade -y
+pkg install nano -y
+pkg install git -y
+pkg install lolcat -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install cowsay -y
+figlet Done |lolcat
+echo
+echo
+echo "      (*) NOW RUN :- bash Mytermux.sh"
+echo
+echo
